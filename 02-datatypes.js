8db9fb7f6eb289847d4a2ef8d3a6eafc4ef634d9
Mijail <mijailwwe@hotmail.com>
2024-12-05 12:16:06 -0500

    Tipos de datos booleanos

--- 02-datatypes.js
+++ 02-datatypes.js
@@ -7,7 +7,9 @@ TIPOS DE DATOS
             1. String > Cadena de texto
             2. Int    > Numeros enteros
             3. Decimal > Numeros decimales
-            
+            4. Boolean > Booleanos
+            5.
+
 
 
 */
@@ -20,3 +22,7 @@ let email = `mijailwwe@gmail.com`
 // Numeros
 let age = 37    // Entero 
 let height = 1.77   // Decimal 
+
+// Booleanos (boolean)
+let isStudent = true
+let isTeacher = false
