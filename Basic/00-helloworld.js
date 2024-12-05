8084b1727ae38bba18a3316b6f08900066f08c45
Mijail <mijailwwe@hotmail.com>
2024-12-05 11:47:59 -0500

    operadores sencillos rapido

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -15,3 +15,11 @@ console.log('Hola JavaScript')
 console.log(`Hola JavaScript`)
 
 */
+
+console.log("5")
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
