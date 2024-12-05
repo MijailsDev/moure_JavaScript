97a346f3a8095eb3c140e45e8aa80f176f730e46
Mijail <mijailwwe@hotmail.com>
2024-12-05 11:43:16 -0500

    formas de imprimir texto en consola

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -1,1 +1,8 @@
-console.log("Hola mundo");
+/* 
+    Las tres formas de imprimir un texto
+*/
+
+// Los puntos y comas no hace falta ponerlas
+console.log("Hola JavaScript")
+console.log('Hola JavaScript')
+console.log(`Hola JavaScript`)
