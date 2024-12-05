d03236932caa8cffd0f1c56128ec1e0f5a640846
Mijail <mijailwwe@hotmail.com>
2024-12-05 11:54:30 -0500

    variables con var

---
+++ Basic/01/variables.js
@@ -0,0 +1,17 @@
+// VARIABLES
+
+    // var          > esta fue la forma original de como se declaraba variables antes (no recomendable)
+var helloWorld = "Hello, JavaScript!"
+console.log(helloWorld)
+    
+var helloWorld = "Hello de nuevo, JavaScript!"
+console.log(helloWorld)
+    
+    
+    // let
+
+    
+    
+    
+    
+    // const
