e51d8ba94e817e9e2600aeb0d7ea1ba887e2431a
Mijail <mijailwwe@hotmail.com>
2024-12-05 12:12:18 -0500

    Tipos de datos tipo String

---
+++ 02-datatypes.js
@@ -0,0 +1,16 @@
+/* 
+TIPOS DE DATOS
+    Son datos basicos e inmutables y representan un solo valor    
+
+    Tipos de datos primitivos:
+        existen 7 tipos:
+            1. String > Cadena de texto
+            2. 
+
+
+*/
+
+// Strings
+let name = "Mijail"
+let alias = 'wolf'
+let email = `mijailwwe@gmail.com`
