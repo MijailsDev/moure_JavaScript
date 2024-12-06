99ce04b8908ad803bc58fcfa4addeed84f971abc
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-06 10:16:35 -0500

    Condicionales else if

--- 08-conditionals.js
+++ 08-conditionals.js
@@ -17,3 +17,18 @@ if (age != 37){
     console.log("La edad es 37 con else")
     
 }
+
+
+
+// else if (sino, si)
+
+if (age != 37){
+    console.log("La edad es DIFERENTE de 37")
+
+}else if(age < 90){
+    console.log("La edad es MENOR que 90")
+
+}else{
+    console.log("La edad es 37 con else")
+    
+}
