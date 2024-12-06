eab1befac7c1cd72fefd88107f0ad6f1099e733f
Mijail <mijailwwe@hotmail.com>
2024-12-05 23:01:34 -0500

    Ejercicios resueltos por yo

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -1,19 +1,61 @@
 // 1. Escribe un comentario en una linea
+// hola yo me llamo goku
 
 // 2. Escribe un comentario en varias lineas
+/*
+Este es un comentarios de
+varias
+lineas 
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de dato primitivo
+let v1="String",v2=3,v3=3.1,v4=true,v5=undefined,v6=null,v7=Symbol("mySymbol"), v8=2121212212121344564567n
 
 // 4. Imprime por consola el valor de todas las variables
-
+console.log(v1)
+console.log(v2)
+console.log(v3)
+console.log(v4)
+console.log(v5)
+console.log(v6)
+console.log(v7)
+console.log(v8)
 // 5. Imprime por consola el tipos de todas las variables
-
+console.log(typeof v1)
+console.log(typeof v2)
+console.log(typeof v3)
+console.log(typeof v4)
+console.log(typeof v5)
+console.log(typeof v6)
+console.log(typeof v7)
+console.log(typeof v8)
 // 6. A continuacion, modifica los valores de las variables por otros del mismo tipo
-
+v1 = "Mi nombre es"
+v2 = 22
+v3 = 1.65
+v4 = false
+v5 = "Definido como un string "
+v6 = "No nulo :)"
+v7 = Symbol("Edit My Symbol")
+v8 = 89798274895728947562n
 // 7. A continuacion, modifica los valores de las variables por otros de distinto tipo
-
+v1 = 12
+v2 = "holaaa"
+v3 = 109
+v4 = null
+v5 = 12
+v6 = false
+v7 = true
+v8 = 0.85
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
-
+const c1="String",c2=3,c3=3.1,c4=true,c5=undefined,c6=null,c7=Symbol("mySymbol"), c8=2121212212121344564567n
 // 9. A continuacion, modifica los valores de las constantes
-
+// c1 = "Hola como estas"
+// c2 = 12
+// c3 = 12.3
+// c4 = false
+// c5 = 123
+// c6 = "hola mundo con JS"
+// c7 = Symbol("Hola symbol")
+// c8 = 2919818798747209292n
 // 10. Comenta las lineas que produzcan algun tipo de error al ejecutarse
