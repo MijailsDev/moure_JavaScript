372be35b045b3096cc26b385ef17e165cca23ae2
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-06 09:14:59 -0500

    Strings - Metodos comunes

--- Basic/06-strings.js
+++ Basic/06-strings.js
@@ -13,3 +13,14 @@ console.log(greeting.length)
 console.log(greeting[0]) // H
 console.log(greeting[1]) // o
 console.log(greeting[12]) // ! :  ultimo caracter
+
+// Metodos comunes
+console.log(greeting.toUpperCase())     // transforma a MAYUSCULAS
+console.log(greeting.toLocaleLowerCase()) // transforma a minudculas
+console.log(greeting.indexOf("Mijail")) // el indice donde a encontrado la palabra Mijail
+console.log(greeting.indexOf("dev")) // -1 : no se encontro esa palabra
+console.log(greeting.includes("Mijail"))  // nos devuel ve un booleano : true/false
+console.log(greeting.includes("dev"))      // nos devuel ve un booleano : true/false
+console.log(greeting.slice(0,10))          // imprime los indices desde el 0 a 10
+console.log(greeting.replace("Mijail", "Goku")) // cambia el valor de Mijail a Goku
+
