11c65467c0086ad0b0f996f518ffd4a3efe16614
Mijail <mijailwwe@hotmail.com>
2024-12-05 23:09:38 -0500

    Operadores aritmeticos

---
+++ Basic/05-operators.js
@@ -0,0 +1,21 @@
+// OPERADORES
+
+// Operadores aritmeticos 
+let a = 5
+let b = 10
+console.log(a + b)     // Suma
+console.log(a - b)     // Resta
+console.log(a * b)     // Multiplicacion
+console.log(a / b)     // Division
+
+console.log(a % b)     // Modulo
+console.log(a ** b)     // Exponentes
+
+a++ // incremento
+console.log(a)
+
+b-- // Decremento
+console.log(b)
+
+
+
