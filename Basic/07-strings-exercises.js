d4e06ac6abc807e103d14d3ae8124b808de51761
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-06 09:41:57 -0500

    Ejercicios de strings

---
+++ Basic/07-strings-exercises.js
@@ -0,0 +1,19 @@
+// 1. Concatena dos cadenas de texto 
+
+// 2. Muestra la longittud de una cadena de texto
+
+// 3. Muestra el primer y ultimo caracter de un string
+
+// 4. Convierte a mayusculas y minusculas un string
+
+// 5. Crea una cadena de texto en varias lineas
+
+// 6. Interpola el valor de una variable en un string
+
+// 7. Reemplaza todos los espacios en blanco de un string por guiones
+
+// 8. Comprueba su una cadena de texto contiene una palabra concretta
+
+// 9. Comprueba si dos strings son iguales
+
+// 10. Comprueba si dos strings tiene la misma longitud
