d480d330ad7cee244b7cb42f863199d4a0bcec0a
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-06 11:15:39 -0500

    Arrays - declaracion e inicializacion

---
+++ Basic/10-arrays.js
@@ -0,0 +1,21 @@
+// ARRAYS
+    
+// Declaracion   >   2 formas posibles: 
+let myArray = []
+let myArray2 = new Array()
+
+// Inicializacion
+myArray = [1]       // almacena el numero 1 
+myArray2 = new Array(3)     // separa 3 espacion vacias en el array
+
+console.log(myArray)
+console.log(myArray2)
+
+
+
+myArray = [1,2,3,"Mijail", true]       // almacena el numero 1 2 3 4 
+myArray2 = new Array(1,2,3,"Mijail", true)     // almacena el numero 1 2 3 4 
+
+
+console.log(myArray)
+console.log(myArray2)
