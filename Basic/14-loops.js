11b75669a415fb86b4c77d41de6b955df649e17c
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-10 10:08:50 -0500

    Bucle While

--- Basic/14-loops.js
+++ Basic/14-loops.js
@@ -10,3 +10,13 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 for (let i = 0; i < numbers.length; i++) {
     console.log(`Elemento: ${numbers[i]}`)
 }
+
+
+// while
+let  i = 0
+while (i < 5) {
+    console.log(`Hola ${i}`)
+    i++
+}
+
+
