06fa15adf4bb55069c6bdcc970bb7491ec81b4ec
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-10 10:06:25 -0500

    Bucle for

---
+++ Basic/14-loops.js
@@ -0,0 +1,12 @@
+// Loops o bucles
+
+// for
+for (let i = 0; i < 5; i++) {
+    console.log(`Hola ${i}`)
+} 
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(`Elemento: ${numbers[i]}`)
+}
