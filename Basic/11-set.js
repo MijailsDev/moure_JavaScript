c6024555749a67de819e549f7a3061d7337b997e
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-10 07:48:54 -0500

    Metodos comunes - delete

--- Basic/11-set.js
+++ Basic/11-set.js
@@ -15,11 +15,16 @@ console.log(mySet)
 // Metodos comunes
 
 //      add : agrega elementos al final del set
-//      delete
+//      delete : elimina los elementos que queremos eliminar
+//               ademas devuelve true si fue eliminado y false si no fue.
 
+// add
 mySet.add("https://moure.dev")
 
 console.log(mySet)
 
 
+// delete
+mySet.delete("https://moure.dev")   // aqui eliminamos "https://moure.dev" del set
 
+console.log(mySet)
