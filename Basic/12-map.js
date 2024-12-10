c668ef8e02f3e87f5149e93257c3d93edf2aa478
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-10 08:33:48 -0500

    Metodos y Propiedades - get

--- Basic/12-map.js
+++ Basic/12-map.js
@@ -21,10 +21,21 @@ console.log(myMap)
 // Metodos y propiedades
 
 //      set : para actualizar o agregar un elemento
-//          
+//      get : devuelve el valor
 
 
 //set 
 myMap.set("alias", "mouredev")      // agregar
 myMap.set("name", "Brais Moure")    // actualizar
 console.log(myMap)
+
+
+
+//get 
+console.log(myMap.get("name"))  // devuelve el valor de la clave name
+console.log(myMap.get("surname"))  // no existe esa clave
+
+
+
+
+
