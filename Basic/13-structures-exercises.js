7c790245d9ab87b37b32ebbffab0d0aec1d3a4a2
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-10 09:49:42 -0500

    Ejercicios de estructuras

---
+++ Basic/13-structures-exercises.js
@@ -0,0 +1,19 @@
+// 1. Crea un array que almacena cinco animales
+
+// 2. Añade dos mas. Uno al principio y otro al final
+
+// 3. Elimina el que se encuentra en tercera posicion
+
+// 4. Crea un set que almacena cinco libros 
+
+// 5. Añade dos mas. Uno de ellos repetido
+
+// 6. Elimina uno concreto a tu eleccion
+
+// 7. Crea un mapa que asocie elnumero del mas a su nombre
+
+// 8. Comprueba si el mes numero 5 existe en el map e imprime su valor
+
+// 9. Añade al mapa una clave con su array como que almacene los meses de verano
+
+// 10. Crea un Array, transformalo a un Set y almacenalo en un Map
