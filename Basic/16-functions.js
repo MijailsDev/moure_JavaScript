429319cc6018ddccd4a3417959575fdc2ca1e8b2
Mijail <mijailwwe@hotmail.com>
2024-12-10 11:22:11 -0500

    Funcion - Declaracion y llamada

---
+++ Basic/16-functions.js
@@ -0,0 +1,13 @@
+// Funciones    :   Es un bloque de codigo para realizar una tarea especifica
+
+// Declaracion 
+function myFunc() {
+    console.log("Hola, funcion!")
+}
+
+// llamada a la funcion
+myFunc()
+myFunc()
+myFunc()
+myFunc()
+myFunc()
