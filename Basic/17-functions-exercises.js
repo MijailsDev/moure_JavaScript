3aa8d1862b0673a90f28aaff9af43236515e0bb2
Mijail <mijailwwe@hotmail.com>
2024-12-10 20:35:31 -0500

    Ejercicios de funciones

---
+++ Basic/17-functions-exercises.js
@@ -0,0 +1,21 @@
+// NOTA: Explora diferentes sintaxis de funciones para resolver los ejercicios
+
+// 1. Crea una funcion que reciba dos numeros y devuelva su suma
+
+// 2. Crea una funcion que reciba un array de numeros y devuelva el mayor de ellos
+
+// 3. Crea una funcion que reciba un string y devuelva el numero de vocales que contienen
+
+// 4. Crea una funcion que reciba un array de strings y devuelva un nuevo array con las strings en mayusculas
+
+// 5. Crea una funcion que reciba un numero y devuelva true si es primo, y false en caso contrario
+
+// 6. Crea una funcion que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
+
+// 7. Crea una funcion que reciba un array de numeros y devuelva la suma de todos los numeros pares
+
+// 8. Crea una funcion que reciba un array de numeros y devuelva un nuevo array con cada numeros elevado al cuadrado
+
+// 9. Crea una funcion que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
+
+// 10. Crea un funcion que calcule el factorial de un numero dado
